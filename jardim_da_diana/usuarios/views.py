68a4df95afdfd89a211f34d52b277b97fe53e3c9
otavio-mmulato <otavio.mmulato@outlook.com.br>
2025-06-04 22:10:10 -0300

    site no repositório da camilly

--- jardim_da_diana/usuarios/views.py
+++ jardim_da_diana/usuarios/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import authenticate, login as login_django
 from django.contrib.auth.decorators import login_required
 
 def cadastro(request):
@@ -11,34 +10,29 @@ def cadastro(request):
     else:
         username = request.POST.get('username')
         email = request.POST.get('email')
-        senha = request.POST.get('senha')
+        senha = request.POST.get('password')  # Corrigido: "password" deve bater com o name do input
 
-        user = User.objects.filter(username=username).first()
-
-        if user:
+        if User.objects.filter(username=username).exists():
             return HttpResponse('Já existe um usuário com esse username')
-        
-        user = User.objects.create_user(username=username, email=email, password=senha)
-        user.save()
 
-        return HttpResponse('Usuário cadastro com sucesso')
+        User.objects.create_user(username=username, email=email, password=senha)
+        return HttpResponse('Usuário cadastrado com sucesso')
 
 def login(request):
     if request.method == 'GET':
-        return render(request, "login.html")
+        return render(request, 'login.html')
     else:
         username = request.POST.get('username')
-        senha = request.POST.get('senha')
+        senha = request.POST.get('password')
 
         user = authenticate(request, username=username, password=senha)
 
         if user:
             login_django(request, user)
-
-            return HttpResponse ('autenticado')
+            return HttpResponse('Autenticado com sucesso')
         else:
-            return HttpResponse ('Email ou senha inválidos')
+            return HttpResponse('Usuário ou senha inválidos')
 
 @login_required(login_url='/auth/login/')
 def plataforma(request):
-    return HttpResponse('Você precisa estar logado')
+    return HttpResponse('Bem-vindo à plataforma! Você está logado.')
